030e0e70055afd6c663d7051d44a57fd110647f3
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-22 18:42:41 +0800

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    // == is equal to (used for comparison)
+    // fe
+    int x,y;
+    printf("Please enter the first number > ");
+    scanf("%d", &x);
+    printf("Please enter the second number > ");
+    scanf("%d", &y);
+    if(x == y){printf("They are equal numbers! \n");}
+    else{printf("They are not the same numbers. \n");}
+    // != is not equal to (used for comparison)
+    // > is greater than (used for comparison)
+    // < is less than (used for comparison)
+    // >= is greater than or equal to (used for comparison)
+    // <= is less than or equal to (used for comparison)
+    // && is and (used for comparison)
+    // || is or (used for comparison)
+    // ! is not (used for comparison)
+    // ? is ternary operator (used for comparison) if true returns left, else returns right 
+    // ?: is ternary operator (used for comparison) checks if it is a null value
+
+
+}
